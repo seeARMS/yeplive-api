d6717a78e3801d58932f6e5406ef21c1048c837c
Evan Marsland <ERmarsland@gmail.com>
2015-04-26 18:56:09 -0400

    updated config

--- config/wowza.php
+++ config/wowza.php
@@ -7,7 +7,7 @@ return [
 		'hdfvr' => '',
 		'vod' => 'rtmp://54.149.106.109/vods3/_definst_/&mp4:amazons3/dev-wowza/',
 		'hlslive_web' => 'http://testing-env-yy.elasticbeanstalk.com:1935/',
-		'upload_mobile' => 'rtmp://testing-env-yy.elasticbeanstalk.com:1935/test/'
+		'upload_mobile' => 'rtmp://54.149.134.14:1935/test/mp4:'
 	],
 
 ];
