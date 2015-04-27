cf1c86f897a1c33ae1d58b2fbec7a8d9864d0eaf
Evan Marsland <ERmarsland@gmail.com>
2015-04-27 00:56:02 -0400

    update config for new wowza

--- config/wowza.php
+++ config/wowza.php
@@ -3,13 +3,14 @@
 return [
 
 	'rtmp' => [
-		'test' => 'rtmp://54.149.106.109:1935/test/_definst_/mp4:',
+		'test' => 'rtmp://52.10.133.244:1935/test/_definst_/mp4:',
 		'hdfvr' => '',
-		'vod' => 'rtmp://54.149.134.14/vods3/_definst_/&mp4:amazons3/dev-wowza/',
-		'hlslive_web' => 'http://testing-env-yy.elasticbeanstalk.com:1935/',
-		'upload_mobile' => 'rtmp://54.149.134.14:1935/test/mp4:',
-		'stream_mp4' => 'https://s3-us-west-2.amazonaws.com/dev-wowza/'
+		'vod' => 'rtmp://52.10.133.244/vods3/_definst_/&mp4:amazons3/dev-wowza/',
+		'hlslive_web' => 'http://52.10.133.244/testing-env-yy.elasticbeanstalk.com:1935/',
+		'upload_mobile' => 'rtmp://52.10.133.244:1935/test/mp4:',
+		'stream_mp4' => 'https://52.10.133.244/dev-wowza/'
 	],
 
-
 ];
+
+rtmp://52.10.133.244:1935/live/_definst_/mp4:TerryChen2
