7e78285cd6fdaf3738a1a34d6965d33f6300c18d
Evan Marsland <ERmarsland@gmail.com>
2015-04-28 14:51:08 -0400

    mobile upload

--- config/wowza.php
+++ config/wowza.php
@@ -9,7 +9,7 @@ return [
 		'hdfvr' => '',
 		'vod' => 'rtmp://52.10.133.244/vods3/_definst_/&mp4:amazons3/dev-wowza/',
 		'hlslive_web' => 'http://52.10.133.244/testing-env-yy.elasticbeanstalk.com:1935/',
-		'upload_mobile' => 'rtmp://52.10.133.244:1935/test/mp4:',
+		'upload_mobile' => 'rtsp://52.10.133.244:1935/test/mp4:',
 		'stream_mp4' => 'https://52.10.133.244/dev-wowza/'
 	],
 	'cloudfront' => [
