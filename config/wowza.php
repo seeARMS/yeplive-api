b64e00318b8edf5cac1141c86b39eb427996c82b
Evan Marsland <ERmarsland@gmail.com>
2015-04-29 12:34:46 -0400

    mp4

--- config/wowza.php
+++ config/wowza.php
@@ -9,7 +9,8 @@ return [
 		'hdfvr' => '',
 		'vod' => 'rtmp://52.10.133.244/vods3/_definst_/&mp4:amazons3/dev-wowza/',
 		'hlslive_web' => 'http://52.10.133.244/testing-env-yy.elasticbeanstalk.com:1935/',
-		'upload_mobile' => 'rtsp://54.149.243.236:1935/liveorigin/mp4:',//'rtsp://52.10.133.244:1935/test/mp4:',
+//		'upload_mobile' => 'rtsp://54.149.243.236:1935/liveorigin/mp4:',//'rtsp://52.10.133.244:1935/test/mp4:',
+		'upload_mobile' => 'rtsp://54.149.243.236:1935/liveorigin/',//'rtsp://52.10.133.244:1935/test/mp4:',
 		'stream_mp4' => 'https://52.10.133.244/dev-wowza/'
 	],
 	'cloudfront' => [
