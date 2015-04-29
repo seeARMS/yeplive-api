8b112b9277d69f402514d4d6e00537a9937d9341
Evan Marsland <ERmarsland@gmail.com>
2015-04-29 02:57:53 -0400

    nice

--- config/wowza.php
+++ config/wowza.php
@@ -5,11 +5,11 @@ return [
 		'host' => 'http://52.10.133.244:8086'
 	],
 	'rtmp' => [
-		'test' => 'rtmp://52.10.133.244:1935/test/_definst_/mp4:',
+		'test' => 'rtmp://wowza-edge-prod-beta.elasticbeanstalk.com/liveedge/_definst_/mp4:',
 		'hdfvr' => '',
 		'vod' => 'rtmp://52.10.133.244/vods3/_definst_/&mp4:amazons3/dev-wowza/',
 		'hlslive_web' => 'http://52.10.133.244/testing-env-yy.elasticbeanstalk.com:1935/',
-		'upload_mobile' => 'rtsp://52.10.133.244:1935/test/mp4:',
+		'upload_mobile' => 'rtsp://54.149.243.236:1935/liveorigin/mp4:',//'rtsp://52.10.133.244:1935/test/mp4:',
 		'stream_mp4' => 'https://52.10.133.244/dev-wowza/'
 	],
 	'cloudfront' => [
