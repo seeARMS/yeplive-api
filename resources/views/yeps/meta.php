4352f488a360f0e2c144424d49f63e3a2ff409b5
Evan Marsland <ERmarsland@gmail.com>
2015-04-29 03:00:40 -0400

    nice

--- resources/views/yeps/meta.php
+++ resources/views/yeps/meta.php
@@ -1,15 +1,12 @@
 <!doctype html>
 <html>
 <head>
-	<meta property="og:title" content="YEPLIVE NOW STREAMING" />
+	<meta property="og:title" content="Yeplive™ | Now Streaming" />
 	<meta property="og:url" content="http://development-vriepmhkv2.elasticbeanstalk.com/api" />
 	<meta property="og:image" content="http://yeplive.com/assets/img/yeplivefontlogo2.png" />
-	<meta property="og:description" content="User is now streaming live on yeplive! Click here to watch!" />
+	<meta property="og:description" content="This " />
 </head>
 <body>
-<?php 
-	echo $yep;
-?>
 hello facebook
 </body>
 </html>
